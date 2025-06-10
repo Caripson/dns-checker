b53136dce6877e6f95334dbc6ffe13138aa4f165
Johan Caripson <31686838+Caripson@users.noreply.github.com>
2025-06-10 12:22:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.getElementById("checkBtn").addEventListener("click", () => {
 
       // List of resolvers considered safe - edit to match your own
       //const trustedResolvers = ['1.2.3.4', 'dns.safesurf.se'];
-      const trustedResolvers = ['192.178.94.20','192.178.94.24','2a00:1450:4025:3c03::127','2a00:1450:4025:3c03::124','2a00:1450:4025:3c05::12a','104.23.222.24','162.158.180.203'];
+      const trustedResolvers = ['192.178.94.20','192.178.94.24','2a00:1450:4025:3c03::127','2a00:1450:4025:3c03::124','2a00:1450:4025:3c05::12a','104.23.222.24','162.158.180.203','162.158.180.203'];
   
 
       // Display message based on whether the resolver is trusted
